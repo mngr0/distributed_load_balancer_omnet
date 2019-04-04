0a84c404350529580504fbd754776151486c390f
mngr0 <ngrmarco@tutanota.com@>
2019-04-04 11:59:43 +0200

    sync

---
+++ out/clang-release/RouterWS.d
@@ -0,0 +1,15 @@
+out/clang-release//RouterWS.o: RouterWS.cc \
+  /home/marco/Space/omnetpp-5.4.1/include/omnetpp.h RouterWS.h \
+  /home/marco/Space/omnetpp-5.4.1/samples/queueinglib/Router.h \
+  /home/marco/Space/omnetpp-5.4.1/samples/queueinglib/QueueingDefs.h \
+  /home/marco/Space/omnetpp-5.4.1/samples/queueinglib/SelectionStrategies.h
+
+/home/marco/Space/omnetpp-5.4.1/include/omnetpp.h:
+
+RouterWS.h:
+
+/home/marco/Space/omnetpp-5.4.1/samples/queueinglib/Router.h:
+
+/home/marco/Space/omnetpp-5.4.1/samples/queueinglib/QueueingDefs.h:
+
+/home/marco/Space/omnetpp-5.4.1/samples/queueinglib/SelectionStrategies.h:
